8e7bbee1a0f4b7d487b5eee16b60c8d2d3cb57a7
Administrator <admin@example.com>
2022-02-12 14:52:17 +0000

    更新 build.sh

--- build.sh
+++ build.sh
@@ -15,3 +15,30 @@ else
 fi
 
 echo "部署完成"
+
+# 发送飞书通知，后续需要写一个服务 集成 自己的 账户体系
+curl --location --request POST 'https://open.feishu.cn/open-apis/bot/v2/hook/d360428e-f5f7-41ae-8f7d-3418298ed99f' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "msg_type": "post",
+    "content": {
+        "post": {
+            "zh_cn": {
+                "title": "项目更新通知",
+                "content": [
+                    [
+                        {
+                            "tag": "text",
+                            "text": "项目有更新: "
+                        },
+                        {
+                            "tag": "a",
+                            "text": "请查看",
+                            "href": "http://192.168.1.5/big_ef/gitlab_test_ci"
+                        }
+                    ]
+                ]
+            }
+        }
+    }
+}'
