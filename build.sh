e94ec1e1ae3e3e07ed46b5c6a8100a0de8da4c7d
Administrator <admin@example.com>
2022-02-12 13:21:37 +0000

    更新 build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,16 @@
 ##############################################################
 # 如果 container 不存在就创建 如果存在就update
 ##############################################################
-docker run update --restart=always -d -p 3002:3002 gitlab-runner-ci-test
+docker stop gitlab-runner-ci
+
+docker container rm -f $( docker ps -aq --no-trunc  --filter ancestor=gitlab-runner-ci-test)
 
 if [[ "$?" -eq '0' ]]; then
+  docker container run --name=gitlab-runner-ci -d -p 3002:3002 gitlab-runner-ci-test
   echo "更新成功"
 else
   docker container run --name=gitlab-runner-ci -d -p 3002:3002 gitlab-runner-ci-test
+  echo "初始化成功"
 fi
+
+echo "部署完成"
